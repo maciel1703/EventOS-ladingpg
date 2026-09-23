8c5f764613ecd7b86280e64feb0092254a0b9a40
Lucas Maciel <lucasmaciel1703@gmail.com>
2026-09-23 15:52:33 -0300

    Ajusta base do Vite para Netlify

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/EventOS-ladingpg/',
+  base: process.env.GITHUB_ACTIONS ? '/EventOS-ladingpg/' : '/',
   plugins: [react()],
 })
